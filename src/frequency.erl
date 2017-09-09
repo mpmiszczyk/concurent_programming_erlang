bc5bbcdb4d4ebab625957c8e857599f75e5a0330
Marcin Miszczyk <mpmiszczyk@gmail.com>
2017-09-09 17:52:50 +0200

    Move from local structure to process.
    Still it is not final implementation, both `allocate/1`,
    `deallocate/2` functions are buggy, but this is not the point.  Idea
    behind this step is to show that one could simply move from local data
    to process without changing interface.  And there are a few notes
       * such change in implementation without modifying interface is possible
       * don't assume that server is singleton, not point, no basis, and it will
       break interface
       * returning Pid (and handling of Pid returns) allows for quick
       process update without having to kill client
       * `init` function is must-have step before `loop`

--- src/frequency.erl
+++ src/frequency.erl
@@ -3,18 +3,47 @@
 -export([
          start/1,
          allocate/1,
-         dealocate/2
+         deallocate/2
+        ]).
+
+-export([
+         loop/1,
+         init/1
         ]).
 
 
-start(ListOfFreqs) ->
-  ListOfFreqs.
-  
 
-allocate(_Freqs = [First|Rest] ) ->
+start(ListOfFreqs) ->
+  spawn(?MODULE, init, [ListOfFreqs]).
+
+allocate(Freqs) ->
+  Freqs ! {allocate, self(), Ref = make_ref()},
+  receive
+    {ok, Freq, Freqs, Ref} ->
+      {ok, Freq, Freqs}
+  end.
+
+deallocate(Freq, Freqs) ->
+  Freqs ! {deallocate, Freq}.
+
+
+
+init(ListOfFreqs) ->
+  ?MODULE:loop({ListOfFreqs, self()}).
+
+loop({Freqs, Server}) ->
+  receive
+    {allocate, Client, Ref} ->
+      {ok, Allocated, RemainingFreqs} = handle_allocate(Freqs),
+      Client ! {ok, Allocated, Server, Ref},
+      ?MODULE:loop({RemainingFreqs, Server});
+    {deallocate, Frequency} ->
+      {ok, NewFreqs} = handle_deallocate(Frequency, Freqs),
+      ?MODULE:loop({NewFreqs, Server})
+  end.
+
+handle_allocate(_Freqs = [First|Rest] ) ->
   {ok, First, Rest}.
 
-dealocate(Freq , Freqs) ->
+handle_deallocate(Freq , Freqs) ->
   {ok, [Freq|Freqs]}.
-
-
